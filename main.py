12aa89ff756873f809d1f8d1627d130ffc4e9792
abeashvil <aa5756@rit.edu>
2024-02-05 13:39:05 -0500

    added html frontend for homepage + leaflet for map

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #main.py
 
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder="C:/Users/thesl/Desktop/ISS-Tracker/templates")
 
 @app.route("/")
-def index():
-    return "We in dis shittttt"
+def home():
+    return render_template("homepage.html")
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
